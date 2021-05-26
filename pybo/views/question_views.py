014474c52c64abb560f1689ac7109c98a7124ac0
jaieve <yoori0303@naver.com>
2021-05-26 23:53:57 +0900

    폼 모듈로 데이터 검증

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -6,7 +6,7 @@ from ..forms import QuestionForm
 from datetime import datetime
 from werkzeug.utils import redirect
 from .. import db
-
+from ..forms import QuestionForm, AnswerForm
 
 
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -20,8 +20,9 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=('POST', 'GET'))
 def create():
