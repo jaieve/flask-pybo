ca424e2ed276724f956dbe543e8f5d5755903386
jaieve <yoori0303@naver.com>
2021-06-03 22:03:23 +0900

    파이보기능 다듬기 210603

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 import config
 # config.py에서 설정해준 다음, 생성한 app에 적용하기 위해 임포트한 config 적용
 
-db = SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
 # create_app()로 app 만들어질때 마다 생성되지 않게 밖에서 선언
 # create_app() 될때 마다 초기화(init_app())
 # db 객체를 create_app 함수 안에서 생성하면 블루프린트와 같은 다른 모듈에서 불러올 수 없다
@@ -16,8 +25,11 @@ def create_app():
 
     #ORM(SQLAlchemy)
     db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     #생성된 모델(models.py의 Question, Answer 클래스)를 migrate기능에 인식시키기
-    migrate.init_app(app,db)
     from . import models
     # Terminal에서 flask db migrate, flask db upgrade 명령 입력하면
     # pybo.db 리비전 파일이 생성되고(SQLite의 데이터베이스파일)
@@ -26,10 +38,12 @@ def create_app():
     #블루프린트
     # 상대경로인 views 폴더에서 3가지 view.py 임포트
     # 플라스트 어플인 app이 생성될 때 각 블루프린트 적용
-    from .views import main_views,question_views, answer_views
+    from .views import main_views,question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
+
 
     # 필터
     from .filter import format_datetime
--- pybo/forms.py
+++ pybo/forms.py
@@ -2,8 +2,9 @@
 # 폼으로 전송된 데이터가 실제 웹 페이지에서 작성된 데이터인지를 판단해주는 역할을하는 CSRF 토큰
 # CSRF토근은 SECRET_KEY를 기반으로 해서 생성된다.
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 # 두 form은 Flas-WTF 모듈의 FlaskForm 클래스를 상속받음
 # FlaskForm 클래스에는 subject와 content 속성이 있음.
@@ -18,3 +19,14 @@ class AnswerForm(FlaskForm):
     # DataRequired : 필수 항목인지 점검
     # Email : 이메일인지 점검
     # Length : 길이를 점검
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
+    email = EmailField('이메일', validators=[DataRequired(), Email()])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
