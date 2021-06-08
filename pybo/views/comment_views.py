48c703c68d546537af470302bf8a652968a25eed
jaieve <yoori0303@naver.com>
2021-06-08 23:23:39 +0900

    질문 댓글 기능 추가 완성

--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -10,12 +10,13 @@ from pybo.views.auth_views import login_required
 
 bp = Blueprint('comment', __name__, url_prefix='/comment')
 
+
 @bp.route('/create/question/<int:question_id>', methods=('GET', 'POST'))
 @login_required
 def create_question(question_id):
-    form = CommentForm
+    form = CommentForm()
     question = Question.query.get_or_404(question_id)
-    if request.method =='POST' and form.validate_on_submit()():
+    if request.method == 'POST' and form.validate_on_submit():
         comment = Comment(user=g.user, content=form.content.data, create_date=datetime.now(), question=question)
         db.session.add(comment)
         db.session.commit()
@@ -24,16 +25,16 @@ def create_question(question_id):
 
 @bp.route('/modify/question/<int:comment_id>', methods=('GET','POST'))
 @login_required
-def modify_comment(comment_id):
+def modify_question(comment_id):
     comment = Comment.query.get_or_404(comment_id)
     if g.user != comment.user:
-        flash('수정권한이 없습니다.')
+        flash('수정권한이 없습니다')
         return redirect(url_for('question.detail', question_id=comment.question.id))
     if request.method == 'POST':
         form = CommentForm()
         if form.validate_on_submit():
             form.populate_obj(comment)
-            comment.modify_date = datetime.now()
+            comment.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
             return redirect(url_for('question.detail', question_id=comment.question.id))
     else:
