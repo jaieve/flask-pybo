b8130b73e8a0db0620a5c2ead528d412ad42546f
jaieve <yoori0303@naver.com>
2021-06-08 20:15:46 +0900

    질문 댓글 수정함수 만들기

--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template, g
+from flask import Blueprint, url_for, request, render_template, g, flash
 from werkzeug.utils import redirect
 
 from pybo import db
@@ -22,4 +22,20 @@ def create_question(question_id):
         return redirect(url_for('question.detail', question_id=question_id))
     return render_template('comment/comment_form.html', form=form)
 
-@bp.route('/modify/question')
+@bp.route('/modify/question/<int:comment_id>', methods=('GET','POST'))
+@login_required
+def modify_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+    if g.user != comment.user:
+        flash('수정권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=comment.question.id))
+    if request.method == 'POST':
+        form = CommentForm()
+        if form.validate_on_submit():
+            form.populate_obj(comment)
+            comment.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=comment.question.id))
+    else:
+        form = CommentForm(obj=comment)
+    return render_template('comment/comment_form.html', form=form)
