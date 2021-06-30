049a7dba7fab086f9116affd1e2e2169912e6f35
jaieve <yoori0303@naver.com>
2021-06-30 22:41:02 +0900

    스크롤 초기화 문제점 해결

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -18,6 +18,7 @@ def question(question_id):
         _question.voter.append(g.user)
         db.session.commit()
     return redirect(url_for('question.detail', question_id=question_id))
+
 @bp.route('/answer/<int:answer_id>/')
 @login_required
 def answer(answer_id):
