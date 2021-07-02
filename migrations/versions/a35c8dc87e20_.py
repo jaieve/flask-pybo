f543fd1cb01dacea464bafef2fda914ac82e6486
jaieve <yoori0303@naver.com>
2021-07-02 17:18:48 +0900

    flask db init 210702

---
+++ migrations/versions/a35c8dc87e20_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a35c8dc87e20
+Revises: 
+Create Date: 2021-07-02 16:59:43.186489
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a35c8dc87e20'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
